814b768483a9c6f8692c3485718c79887eefc3d8
Marco <mvelar@uni.pe>
2019-11-20 17:56:01 -0500

    Submission model Representa los envios de formulario del censo, se le asocia una direccion ip y un registro del miembro insertado, relacion conecta.

---
+++ routes/api/models/submission.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../../../config/database');
+const Miembro = require('../models/miembro');
+class Submission extends Model {}
+
+Submission.init({
+    direccionIp: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    idEntradaMiembro: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Miembro,
+            key: 'id'
+        }
+    }
+},{
+    sequelize,
+    modelName: 'submission',
+    updatedAt: false,
+    freezeTableName: true,
+    comment: 'La tabla submission contiene informacion sobre los envios de formularios'
+});
+Submission.hasOne(Miembro);
+Submission.sync();
+
+module.exports = Submission;
